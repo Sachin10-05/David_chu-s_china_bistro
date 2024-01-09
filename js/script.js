74798c8a2fbce52ea965dfc670b28996a58719d5
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2024-01-09 07:15:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(function()
+{
+  $("#navbarToggle").blur(function(event)
+  {
+    let screenWidth = window.innerWidth;
+    if (screenWidth < 768)
+    {
+      $("#navbarNavAltMarkup").collapse('hide');
+    }
+  });
+});
+
+
+
+
+
+
+//Load Home snippet
+(function (global)
+{
+  let dc = {};
+  let homeHtml = "snippets/home-snippet.html";
+
+  //Insert html for select:
+  let insertHtml = function(html, selector)
+  {
+    let targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+
+  //SHOW Loading Icon:
+  let showLoading = function (selector) 
+  {
+    let html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
+
+
+
+  //On page load (before images or CSS)
+  document.addEventListener("DOMContentLoaded", function (event) 
+  {
+    // On first load, show home view
+    showLoading$("#main-content");
+    $ajaxUtils.sendGetRequest(homeHtml, function (responseText) 
+    {
+      $document.querySelector("#main-content").innerHTML = responseText;
+    }, false);
+  });
+  
+  global.$dc= dc;
+  
+}(window));
